a044b8f80cafb6a00babb6e024e56a0a5ae63708
Jericko24 <jerickochavez24@gmail.com>
2024-06-10 10:34:04 -0600

    Added and completed week 4 assignment

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -2,10 +2,11 @@ import { NewItem } from "./new-item";
 
 export default function Page() {
     return (
-        <main>
-            <header>
-                <NewItem/>
+        <main className="bg-slate-400 py-4 text-center">
+            <header className="text-2xl">
+                <h1> Week 4 Assignment </h1>
             </header>
+            <NewItem /> 
         </main>
     );
 }
